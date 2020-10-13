ec3dc66f73e53b779eb49035855dfb83df3026ce
martin <martinaguirre08@gmail.com>
2020-10-13 00:44:07 -0300

    quite el comp borrado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcmeProductsComponent } from './acme-products/acme-products.component';
-import { CatalogComponent } from './catalog/catalog.component';
 import { MailOrderComponent } from './mail-order/mail-order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AcmeProductsComponent,
-    CatalogComponent,
     MailOrderComponent
   ],
   imports: [
